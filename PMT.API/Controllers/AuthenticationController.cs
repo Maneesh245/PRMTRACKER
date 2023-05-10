190673e184c3cd60e973f69af97df76a5c431a06
cogdotnet1618 <cogdotnet1618@CTSDOTNET973>
2023-05-10 17:26:38 +0530

    login test

--- PMT.API/Controllers/AuthenticationController.cs
+++ PMT.API/Controllers/AuthenticationController.cs
@@ -41,5 +41,30 @@ namespace PMT.API.Controllers
         
 
     }
+        [HttpGet("login1")]
+        public IActionResult Login()
+        {
+
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenkey = Encoding.UTF8.GetBytes("c87ebd3f-c4d8-4f15-81db-4f94363a2038");
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                    new Claim(ClaimTypes.Email, "admin@gmail.com")
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(10),
+                    SigningCredentials =
+                    new SigningCredentials(
+                        new SymmetricSecurityKey(tokenkey),
+                        SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return Ok(new { status = "Ok", role ="MemberAccessException", token = tokenHandler.WriteToken(token) });
+          
+
+
+        }
     }
 }
